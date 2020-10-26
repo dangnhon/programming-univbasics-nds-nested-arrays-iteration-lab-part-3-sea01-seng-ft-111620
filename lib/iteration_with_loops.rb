56df9366e10f2c19a577f02faf7d169a18716540
Nhon Dang <dang.nhon12345@yahoo.com>
2020-10-26 17:58:27 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ while row < src.length do
   element = 0
   strings_only = ""
   while element < src[row].length do
-    if src[row][element] == ""
+    if src[row][element].length > strings_only.length
       strings_only = src[row][element]
     end
     element += 1
